9cdb422134f58cd1bb2c56aca9a6bfe1fada4fa9
AdrianEddy <adrian.eddy@gmail.com>
2022-07-11 17:44:06 +0200

    Update missing file

--- src/dji/mod.rs
+++ src/dji/mod.rs
@@ -27,7 +27,7 @@ impl Dji {
 
     pub fn parse<T: Read + Seek, F: Fn(f64)>(&mut self, stream: &mut T, size: usize, _progress_cb: F, cancel_flag: Arc<AtomicBool>) -> Result<Vec<SampleInfo>> {
         let mut samples = Vec::new();
-        util::get_other_track_samples(stream, size, |mut info: SampleInfo, data: &[u8], file_position: u64| {
+        util::get_other_track_samples(stream, size, true, |mut info: SampleInfo, data: &[u8], file_position: u64| {
             if let Ok(parsed) = dbgi::DebugInfoMain::decode(data) {
                 if let Some (frame) = parsed.frames.first() {
 
