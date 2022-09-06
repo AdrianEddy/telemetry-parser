e371886944469187c2b64cd457025b83fcccf764
Adrian <adrian.eddy@gmail.com>
2022-09-06 21:26:14 +0200

    Release v0.2.2

--- bin/gyro2bb/gyro2bb.rs
+++ bin/gyro2bb/gyro2bb.rs
@@ -5,7 +5,7 @@ use std::sync::{ Arc, atomic::AtomicBool };
 use telemetry_parser::*;
 use telemetry_parser::tags_impl::*;
 
-/** gyro2bb v0.2.1
+/** gyro2bb v0.2.2
 Author: Adrian <adrian.eddy@gmail.com>
 
 Extract gyro data from Sony, GoPro and Insta360 cameras to betaflight blackbox csv log
